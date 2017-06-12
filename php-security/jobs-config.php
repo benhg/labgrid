3fd19be5b05d24bc86762fdaedfef4e6bb7249d6
Ben Glick <benhg@users.noreply.github.com>
2017-06-12 16:31:10 -0500

    Create jobs-config.php

--- php-security/jobs-config.php
+++ php-security/jobs-config.php
@@ -3,5 +3,5 @@
 // These are the database login details
 define("JOBHOST", "localhost");
 define("JOBUSER", "root");
-define("JOBPASSWORD", "bakersdozen");
+define("JOBPASSWORD", "JOB PASS");
 define("JOBDATABASE", "labgrid");
