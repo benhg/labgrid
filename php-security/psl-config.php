935fca2ea5c851d4c54b96b76637ce56bbbe507b
Ben Glick <benhg@users.noreply.github.com>
2017-06-12 16:31:39 -0500

    Create psl-config.php

--- php-security/psl-config.php
+++ php-security/psl-config.php
@@ -3,8 +3,8 @@
 // These are the database login details
 define("HOST", "localhost");
 define("USER", "sec_user");
-//define("PASSWORD", "rohzeiKie8ohN1Reeth0Ieth9eineiseepa7eemiwah8tuhahniimah5geij4jee");
-define("PASSWORD", "rohzeiKie8ohN1Reeth0Ieth9eineiseepa7");
+
+define("PASSWORD", "PASSWORD");
 define("DATABASE", "secure_login");
 
 // Session specifications
